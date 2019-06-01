c0c728412c0960e456b84e91b44a29d36af57783
Robert Gant <demontegant@gmail.com>
2019-06-01 12:15:44 -0500

    added everything i needed and i also converted the css to styled componets

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const LambdaHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    .header {
+      width: 1280px;
+    }
+  }
+`;
+const DateCss = styled.header`
+margin-left: 25px;
+  flex: 1;
+`;
+
+const H1Css = styled.header`
+ font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const TempHeader = styled.header`
+ text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <LambdaHeader> 
+      <DateCss>SMARCH 32, 2018</DateCss>
+      <H1Css>Lambda Times</H1Css>
+      <TempHeader>98°</TempHeader>
+    </LambdaHeader>
   )
 }
 
